da7db0114d1387bdb18b03a631fc97cac8941bc4
Ubean <ubeannn@gmail.com>
2021-11-22 16:57:52 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/latihan_navigation_4/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.dicoding.latihan_navigation_4.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
